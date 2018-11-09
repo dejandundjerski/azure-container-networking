ca50d9f1fa727e5204378c80e85f19eb5570eae9
Ashvin Deodhar <ashvin.deodhar@gmail.com>
2018-11-09 12:25:50 -0800

    Fix endpoint policy regression
    Handle the OutBoundNAT policy with a dedicated logic during
    serializing the policies

--- network/policy/policy.go
+++ network/policy/policy.go
@@ -2,7 +2,10 @@ package policy
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+
+	"github.com/Microsoft/hcsshim"
 )
 
 const (
@@ -19,11 +22,17 @@ type Policy struct {
 }
 
 // SerializePolicies serializes policies to json.
-func SerializePolicies(policyType CNIPolicyType, policies []Policy) []json.RawMessage {
+func SerializePolicies(policyType CNIPolicyType, policies []Policy, epInfoData map[string]interface{}) []json.RawMessage {
 	var jsonPolicies []json.RawMessage
 	for _, policy := range policies {
 		if policy.Type == policyType {
-			jsonPolicies = append(jsonPolicies, policy.Data)
+			if isPolicyTypeOutBoundNAT := IsPolicyTypeOutBoundNAT(policy); isPolicyTypeOutBoundNAT {
+				if serializedOutboundNatPolicy, err := SetOutBoundNATPolicy(policies, epInfoData); err == nil {
+					jsonPolicies = append(jsonPolicies, serializedOutboundNatPolicy)
+				}
+			} else {
+				jsonPolicies = append(jsonPolicies, policy.Data)
+			}
 		}
 	}
 	return jsonPolicies
@@ -54,6 +63,59 @@ func GetOutBoundNatExceptionList(policies []Policy) ([]string, error) {
 		}
 	}
 
-	log.Printf("OutBoundNAT policy not set.")
+	log.Printf("OutBoundNAT policy not set")
 	return nil, nil
+}
+
+// IsPolicyTypeOutBoundNAT return true if the policy type is OutBoundNAT
+func IsPolicyTypeOutBoundNAT(policy Policy) bool {
+	if policy.Type == EndpointPolicy {
+		type KVPair struct {
+			Type          CNIPolicyType   `json:"Type"`
+			ExceptionList json.RawMessage `json:"ExceptionList"`
+		}
+		var data KVPair
+		if err := json.Unmarshal(policy.Data, &data); err != nil {
+			return false
+		}
+
+		if data.Type == OutBoundNatPolicy {
+			return true
+		}
+	}
+
+	return false
+}
+
+// SetOutBoundNATPolicy formulates OutBoundNAT policy and returns serialized json
+func SetOutBoundNATPolicy(policies []Policy, epInfoData map[string]interface{}) (json.RawMessage, error) {
+	outBoundNatPolicy := hcsshim.OutboundNatPolicy{}
+	outBoundNatPolicy.Policy.Type = hcsshim.OutboundNat
+
+	exceptionList, err := GetOutBoundNatExceptionList(policies)
+	if err != nil {
+		log.Printf("Failed to parse outbound NAT policy %v", err)
+		return nil, err
+	}
+
+	if exceptionList != nil {
+		for _, ipAddress := range exceptionList {
+			outBoundNatPolicy.Exceptions = append(outBoundNatPolicy.Exceptions, ipAddress)
+		}
+	}
+
+	if epInfoData["cnetAddressSpace"] != nil {
+		if cnetAddressSpace := epInfoData["cnetAddressSpace"].([]string); cnetAddressSpace != nil {
+			for _, ipAddress := range cnetAddressSpace {
+				outBoundNatPolicy.Exceptions = append(outBoundNatPolicy.Exceptions, ipAddress)
+			}
+		}
+	}
+
+	if outBoundNatPolicy.Exceptions != nil {
+		serializedOutboundNatPolicy, _ := json.Marshal(outBoundNatPolicy)
+		return serializedOutboundNatPolicy, nil
+	}
+
+	return nil, fmt.Errorf("OutBoundNAT policy not set")
 }
